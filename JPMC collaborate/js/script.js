9db9952239372b2ef5d4a4427f774b2cccd85cc5
keswanisandeep <deepnam82@gmail.com>
2019-06-24 23:37:33 +0530

    Changes for Post to work
    Changes for Post to work

--- JPMC collaborate/js/script.js
+++ JPMC collaborate/js/script.js
@@ -1,9 +1,74 @@
-var myApp = angular.module('myApp', ['infinite-scroll',  ,'ngRoute']);
+var myApp = angular.module('myApp', ['infinite-scroll','ui.bootstrap','ngRoute','ngSanitize', 'ui.select']);
 
 myApp.controller('DemoController', function($scope, GHRepo) {
   $scope.ghRepo = new GHRepo();
 });
 
+myApp.controller("MainController",['$scope','$uibModal',function($scope,$uibModal){ 
+ $scope.openModal = function(){
+	 $scope.modalInstance = $uibModal.open({
+	 ariaLabelledBy: 'modal-title',
+	 ariaDescribedBy: 'modal-body',
+	 templateUrl: 'modalWindow.html',
+	 controller :'ModelHandlerController',
+	 controllerAs: '$ctrl',
+	 size: 'lg',
+	 resolve: {
+	 
+	 }
+	 });
+
+ }
+ 
+}]);
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+myApp.controller("ModelHandlerController",function($scope,$uibModalInstance){
+	$scope.itemArray = [
+        {id: 1, name: 'Real Estate Buy/Sell/Rent'},
+        {id: 2, name: 'Carpool'},
+        {id: 3, name: 'Vehicles Buy/Sell/Rent'},
+        {id: 4, name: 'Sports'},
+        {id: 5, name: 'Gigs/Concerts'},
+        {id: 6, name: 'Ask'},
+        {id: 7, name: 'Misc'},
+    ];
+	$scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+        var uploadUrl = "/fileUpload";
+        alert(file.name + " Uploaded successfully")
+        $scope.myFile = '';
+        $scope.model.imageName=file.name
+        //fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+    //
+    $scope.model = {selectedCategory :[]};
+    
+ $scope.cancelModal = function(){
+	 $uibModalInstance.dismiss('close');
+ }
+ $scope.ok = function(){
+	 alert('Your Post has been uploaded');
+	 $uibModalInstance.close('save');
+ 
+ }
+ 
+});
 // Reddit constructor function to encapsulate HTTP and pagination logic
 myApp.factory('GHRepo', function($http) {
   var GHRepo = function() {
